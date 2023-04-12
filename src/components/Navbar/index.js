ef8377f92abc5a970d1d3a1f889147ce4ead9adb
folajhimie <blackhemisphere12@gmail.com>
2023-04-12 02:02:55 +0100

    Jobber website Update...

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,16 @@
-import classNames from 'classnames';
-import React, { useEffect, useState } from 'react';
-import { BsList, BsX } from 'react-icons/bs';
-import { useMediaQuery } from 'react-responsive';
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
+import { BsList, BsX } from "react-icons/bs";
+import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
 
 // import logo from '../../images/logo.svg';
 // import PrimaryButton from '@/components/buttons/PrimaryButton';
 // import PrimaryButton from '../buttons/PrimaryButton';
-import Menus from './Menus';
+import Menus from "./Menus";
 // import SecondaryButton from '../buttons/SecondaryButton';
-import JobberLogo from '../../assets/images/fontIcon/icon.png'
-import Icon from '../../assets/images/fontIcon/logo.png'
+import JobberLogo from "../../assets/images/fontIcon/icon.png";
+import Icon from "../../assets/images/fontIcon/logo.png";
 // import SecondaryButton from '@/components/buttons/SecondaryButton';
 // import Menus from './Menus';
 
@@ -17,7 +18,7 @@ export default function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [backgroundwhite, setBackgroundWhite] = useState(false);
 
-  const isMobile = useMediaQuery({ maxWidth: '768px' });
+  const isMobile = useMediaQuery({ maxWidth: "768px" });
 
   const handleWindowScroll = (e) => {
     const height = window.scrollY;
@@ -46,71 +47,91 @@ export default function Navbar() {
   }, [dropdownOpen]);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleWindowScroll);
+    window.addEventListener("scroll", handleWindowScroll);
 
-    return () => window.removeEventListener('scroll', handleWindowScroll);
+    return () => window.removeEventListener("scroll", handleWindowScroll);
   }, []);
 
   return (
-    <nav className={classNames('fixed w-full transition-all duration-700 z-10 py-8 ', {
-      "bg-white shadow-lg !py-3": backgroundwhite
-    })}>
+    <nav
+      className={classNames(
+        "fixed w-full transition-all duration-700 z-10 py-8 ",
+        {
+          "bg-white shadow-lg !py-3": backgroundwhite,
+        }
+      )}
+    >
       <div className="px-4 mx-auto top-0 flex justify-between items-center">
-        <a href="/landing" className="flex items-center">
-          {/* <img src={logo} className="mr-10" alt="Jobber" /> */}
-          <div className="flex justify-center w-full ">
-            <div
-              className="m-1"
-              style={{ width: "30px", height: "30px" }}
-            >
-
+        <div className="flex items-center">
+          <Link className="link flex items-center" to="/">
+            <div className="m-1" style={{ width: "30px", height: "30px" }}>
               <img src={JobberLogo} alt="jobber" width="80px" height="80px" />
-
             </div>
-            <span className="mt-1 mr-6"><img src={Icon} alt="jobber" width="140px" height="140px" /></span>
-          </div>
-        </a>
-        <div className=' hidden w-5/12 mx-4 gap-8 md:hidden xl:flex sm:hidden lg:hidden xsl:flex'>
+            <span className="mt-1 mr-6">
+              <img src={Icon} alt="jobber" width="140px" height="140px" />
+            </span>
+          </Link>
+        </div>
+        <div className=" hidden w-5/12 mx-4 gap-8 md:hidden xl:flex sm:hidden lg:hidden xsl:flex">
           <Menus />
         </div>
         <div className="hidden gap-4 md:flex">
           {/* <SecondaryButton>
             
           </SecondaryButton> */}
-          <a className="bg-primary rounded-md py-2 px-10 text-white font-poppins flex items-center" href='/auth/login' alt="">Sign In</a>
+          <Link className="link flex items-center" to="/auth/login">
+            <div className="bg-primary rounded-md py-2 px-10 text-white font-poppins flex items-center">
+              Sign In
+            </div>
+          </Link>
           {/* <button >
                 Learn More
               </button> */}
-          <a className="bg-white border border-teal-700 rounded-md py-2 px-10 text-primary font-poppins flex items-center" href='/auth/register' alt="">Sign Up</a>
+          <Link className="link flex items-center" to="/auth/register">
+            <div className="bg-white border border-teal-700 rounded-md py-2 px-10 text-primary font-poppins flex items-center">
+              Sign Up
+            </div>
+          </Link>
           {/* <PrimaryButton>
           
             <a href='/auth/register' alt="">Sign up</a>
           </PrimaryButton> */}
         </div>
         <div className="md:hidden text-2xl">
-          <button className="z-50 p-4 block transition-all" onClick={() => setDropdownOpen(!dropdownOpen)}>
-            {dropdownOpen ?
-              <BsX />
-              :
-              <BsList />
-            }
+          <button
+            className="z-50 p-4 block transition-all"
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+          >
+            {dropdownOpen ? <BsX /> : <BsList />}
           </button>
 
           {/* Menu dropdown */}
-          <div className={classNames({
-            'text-base left-0 top-full right-0 absolute transition-all duration-400': true,
-            'invisible opacity-0': !dropdownOpen,
-            'visible opacity-100': dropdownOpen,
-          })}>
-            <div className="h-screen left-0 bg-black bg-opacity-30" onClick={handleBlackScreenClick}>
+          <div
+            className={classNames({
+              "text-base left-0 top-full right-0 absolute transition-all duration-400": true,
+              "invisible opacity-0": !dropdownOpen,
+              "visible opacity-100": dropdownOpen,
+            })}
+          >
+            <div
+              className="h-screen left-0 bg-black bg-opacity-30"
+              onClick={handleBlackScreenClick}
+            >
               <div className="z-20 shadow-xl bg-white p-6">
                 <div className="mb-4">
                   <Menus />
                 </div>
                 <div className="gap-4 flex mb-6 ml-6">
-
-                  <a className="bg-primary rounded-md py-2 px-10 text-white font-poppins flex items-center" href='/auth/login' alt="">Sign In</a>
-                  <a className="bg-white border border-teal-700 rounded-md py-2 px-10 text-primary font-poppins flex items-center" href='/auth/register' alt="">Sign Up</a>
+                  <Link className="link flex items-center" to="/auth/login">
+                    <div className="bg-primary rounded-md py-2 px-10 text-white font-poppins flex items-center">
+                      Sign In
+                    </div>
+                  </Link>
+                  <Link className="link flex items-center" to="/auth/register">
+                    <div className="bg-white border border-teal-700 rounded-md py-2 px-10 text-primary font-poppins flex items-center">
+                      Sign Up
+                    </div>
+                  </Link>
                   {/* <SecondaryButton className="w-full">
                     Sign In
                   </SecondaryButton>
